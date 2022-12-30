c91772b7395414e7545167421dcbe561194ed1bf
Narain Singaram <narain.awesome@gmail.com>
2022-12-30 17:34:44 -0500

    Fixed error and created basic html page

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = pd.read_csv("data_small/TG_STAID " + str(station).zfill(6) + ".txt")
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == "1860-01-05"]['   TG'].squeeze() / 10
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {
         "station": station,
         "date": date,
